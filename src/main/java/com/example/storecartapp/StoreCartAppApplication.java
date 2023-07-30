fdedd14001e0c22a9d7c616939f10d0dffcdddf0
lamiyahell <axira1108@gmail.com>
2023-07-30 14:57:13 +0300

    1 commit

---
+++ src/main/java/com/example/storecartapp/StoreCartAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.storecartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCartAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCartAppApplication.class, args);
+    }
+
+}
